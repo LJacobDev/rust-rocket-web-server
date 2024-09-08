40a0cdb6d4bd25c90e2efe468a6a76ba8e3a2f64
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-09-08 18:49:41 -0400

    Add table creation sql statement

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 //which turns the fn index() that it precedes into an endpoint
 #![feature(proc_macro_hygiene, decl_macro)]
 
-
 //'macro_use' here is specifying that all the macros and decorator functions in Rocket should be imported into the project
 //this is how we get access to 'get()' in the decorator below here
 #[macro_use]
@@ -20,13 +19,27 @@ fn index() -> &'static str {
 
 #[launch]
 fn launch_rocket() -> _ {
+    println!("Starting server");
+    println!("Initializing database");
 
-        //create sqlite database
-        let db_connection = rusqlite::Connection::open("data.sqlite");
+    //create sqlite database
+    let db_connection = rusqlite::Connection::open("data.sqlite")
+        .expect("Database connection failed, terminating program");
 
-        // rocket::ignite().mount("/", routes![index].launch());
-        rocket::build().mount("/", routes![index])
-    
+    //this method takes in a sql string and a params collection
+    //because no params are needed in this sql statement, the params argument can be given () or [].
+    //It used to be possible to use rusqlite::NO_PARAMS but that has since been removed
+    //and &[] isn't allowed in this example
+    let connection_result = db_connection.execute(
+        "create table if not exists test (id integer primary key);",
+        //&[],                  //not permitted
+        //rusqlite::NO_PARAMS   //has been removed from rusqlite
+        // [],                  //permitted
+        (),                     //also permitted
+    );
+
+    println!("Connection result: {:?}", connection_result);
+
+    // rocket::ignite().mount("/", routes![index].launch());
+    rocket::build().mount("/", routes![index])
 }
-
-
